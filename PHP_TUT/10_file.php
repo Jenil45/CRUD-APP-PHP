78d42b144d489972416319eeb3d579cd259fdcbc
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-24 23:43:31 +0530

    Add files via upload

---
+++ PHP_TUT/10_file.php
@@ -0,0 +1,6 @@
+<?php
+
+// can read any file .jpg , .png , .html etc.
+$b = readfile("myFile.txt");                                   // print content of file
+echo $b;                                                       // print length of content
+?>
